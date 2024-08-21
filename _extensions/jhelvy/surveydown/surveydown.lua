70cc27e4f83784aa3b907cb2696cf9f577b41f9c
Pingfan Hu <pingfan0727@gmail.com>
2024-08-20 22:04:52 -0400

    update to v0.1.1

--- _extensions/jhelvy/surveydown/surveydown.lua
+++ _extensions/jhelvy/surveydown/surveydown.lua
@@ -1,7 +1,40 @@
+-- Function to get the path of a file in an R package
+local function get_package_file_path(package_name, file_name, subdirectory)
+  local cmd = string.format(
+    "Rscript -e \"cat(system.file('%s', '%s', package = '%s'))\"",
+    subdirectory or "", file_name, package_name
+  )
+  return pandoc.pipe("sh", {"-c", cmd}, ""):gsub("%s+$", "")
+end
+
 local function ensure_html_deps()
+  local package_name = "surveydown"
+
+  local surveydown_css_path = get_package_file_path(package_name, "surveydown.css", "css")
+  local page_nav_js_path = get_package_file_path(package_name, "page_nav.js", "js")
+  local keep_alive_js_path = get_package_file_path(package_name, "keep_alive.js", "js")
+  local update_progress_js_path = get_package_file_path(package_name, "update_progress.js", "js")
+  local required_questions_js_path = get_package_file_path(package_name, "required_questions.js", "js")
+
   quarto.doc.add_html_dependency({
     name = 'surveydowncss',
-    stylesheets = {"surveydown.css"}
+    stylesheets = {surveydown_css_path}
+  })
+  quarto.doc.add_html_dependency({
+    name = 'pagenavjs',
+    scripts = {page_nav_js_path}
+  })
+  quarto.doc.add_html_dependency({
+    name = 'keepalivejs',
+    scripts = {keep_alive_js_path}
+  })
+  quarto.doc.add_html_dependency({
+    name = 'updateprogressjs',
+    scripts = {update_progress_js_path}
+  })
+  quarto.doc.add_html_dependency({
+    name = 'requiredquestionsjs',
+    scripts = {required_questions_js_path}
   })
 end
 
